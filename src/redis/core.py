6701879a3cc829e6252a8c92c33cbb71d7726d49
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2015-05-28 16:14:41 +1200

    minor changes

--- src/redis/core.py
+++ src/redis/core.py
@@ -8,8 +8,14 @@ import re
 from datetime import datetime, timedelta
 import uuid
 
+class Key(object):
+    @staticmethod
+    def hashkey(eid, created):
+        combo = "%s#%s"%(eid, created)
+        return uuid.uuid3(uuid.NAMESPACE_DNS, combo).hex
+
 class User(object):
-    def __init__(self, eid, name, email, created=time.time(), hashkey=None):
+    def __init__(self, eid, name, email, created=time.time()):
         if type(created) is not FloatType:
             raise CoreError("[FloatType] required, passed [%s]"%(type(created)))
         self._id = str(eid).lower()
@@ -18,28 +24,21 @@ class User(object):
         # created as a timestamp instance
         self._created = created
         # create hash for projects
-        self._hash = hashkey if hashkey else User.hashkey(self._id, self._created)
-
-    @staticmethod
-    def hashkey(eid, created):
-        combo = "%s#%s"%(eid, created)
-        return uuid.uuid3(uuid.NAMESPACE_DNS, combo).hex
+        self._hash = Key.hashkey(self._id, self._created)
 
     @classmethod
     def create(cls, settings):
         if type(settings) is not DictType:
             raise CoreError("[DictType] required, passed [%s]"%(type(settings)))
         created = settings["_created"] if "_created" in settings else None
-        hashkey = settings["_hash"] if "_hash" in settings else None
-        return User(settings["_id"], settings["_name"], settings["_email"], created, hashkey)
+        return User(settings["_id"], settings["_name"], settings["_email"], created)
 
     def dict(self):
         return {
             "_id": self._id,
             "_name": self._name,
             "_email": self._email,
-            "_created": self._created,
-            "_hash": self._hash
+            "_created": self._created
         }
 
     def id(self):
@@ -65,6 +64,8 @@ class Project(object):
         self._name = str(name) if name else ""
         # created as a timestamp instance
         self._created = created
+        # create hash for branches
+        self._hash = Key.hashkey(self._id, self._created)
 
     @staticmethod
     def MIN_ID_LENGTH():
@@ -83,6 +84,9 @@ class Project(object):
 
     def id(self):
         return self._id
+
+    def hash(self):
+        return self._hash
 
     def datetime(self, offset=None, template="%d/%m/%Y %H:%M:%S"):
         utc = datetime.utcfromtimestamp(self._created)
@@ -114,9 +118,9 @@ class Project(object):
 class Branch(object):
     def __init__(self, name, created=time.time(), edited=time.time()):
         self._name = str(name).strip()
-        self._id = Branch.hashid(self._name)
         self._created = created
         self._edited = edited
+        self._hash = Key.hashkey(self._name, self._created)
 
     @classmethod
     def create(cls, settings):
@@ -126,15 +130,11 @@ class Branch(object):
         edited = settings["_edited"] if "_edited" in settings else None
         return Branch(settings["_name"], created, edited)
 
-    @staticmethod
-    def hashid(self, eid):
-        return uuid.uuid3(uuid.NAMESPACE_DNS, eid).hex
-
     def name(self):
         return self._name
 
-    def id(self):
-        return self._id
+    def hash(self):
+        return self._hash
 
     def created(self):
         return self._created
